0a3e80262ee5eee695ee5e5ab53ccb9ca8091a03
Konrad Blicharz <konradcodec@gmail.com>
2023-01-06 13:41:33 +0100

    fixing bug with hotel icon

--- src/edu/accomodation/Main.java
+++ src/edu/accomodation/Main.java
@@ -7,12 +7,7 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException, IOException {
-
-
-
-
-
+    public static void main(String[] args) throws IOException {
 
         //formatka
         new MainPanel("Accomodation").setVisible(true);
--- src/edu/accomodation/Waypoints/ButtonWaypoint.java
+++ src/edu/accomodation/Waypoints/ButtonWaypoint.java
@@ -7,7 +7,7 @@ public class ButtonWaypoint extends JButton {
     public ButtonWaypoint() {
         setContentAreaFilled(false);
         setCursor(new Cursor(Cursor.HAND_CURSOR));
-        setIcon(new ImageIcon(getClass().getResource("/icon/hotel.png")));
+        setIcon(new ImageIcon("BGM_v2/src/edu/accomodation/Waypoints/hotel.png"));
         setBorder(null);
         setSize(new Dimension(24, 24));
     }
